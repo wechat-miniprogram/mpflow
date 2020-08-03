3fdd2427d71155971471ae10edb27180beaf4911
chyizheng <chyizheng@tencent.com>
2020-08-03 20:32:04 +0800

    fix(template-miniprogram-plugin): fix template

--- packages/template-miniprogram-plugin/template/weflow.config.js
+++ packages/template-miniprogram-plugin/template/weflow.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   appId: '<%= appId %>',
-  app: process.env.BUILD_APP ? 'src/miniprogram/app' : undefined,
+  app: (mode) => (mode !== "production" ? "src/miniprogram/app" : undefined),
   plugin: 'src/plugin/plugin',
   compileType: 'plugin',
   miniprogramRoot: 'miniprogram',
