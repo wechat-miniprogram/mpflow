5fa4da837c4f4193e7af9bc51390571ce9c0e28f
chyizheng <chyizheng@tencent.com>
2020-09-10 20:15:18 +0800

    feat(plugin-babel): use core-js 2

--- packages/plugin-babel/preset.js
+++ packages/plugin-babel/preset.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   return {
-    presets: [[require.resolve('@babel/preset-env'), { targets: { chrome: '67' }, useBuiltIns: 'usage', corejs: 3 }]],
+    presets: [[require.resolve('@babel/preset-env'), { targets: { chrome: '67' }, useBuiltIns: 'usage', corejs: 2 }]],
     env: {
       test: {
         presets: [[require.resolve('@babel/preset-env'), { targets: { node: 'current' } }]],
