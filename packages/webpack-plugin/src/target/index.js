3e8244a4dfa2395bffffa369129357340e92ad87
chyizheng <chyizheng@tencent.com>
2020-11-12 20:59:59 +0800

    fix(webpack-plugin): support private api

--- packages/webpack-plugin/src/target/index.js
+++ packages/webpack-plugin/src/target/index.js
@@ -1,9 +1,11 @@
 import FunctionModulePlugin from 'webpack/lib/FunctionModulePlugin'
 import LoaderTargetPlugin from 'webpack/lib/LoaderTargetPlugin'
+import ExternalsPlugin from 'webpack/lib/ExternalsPlugin'
 import MiniprogramTemplatePlugin from './MiniprogramTemplatePlugin'
 
 export default function MiniProgramTarget(compiler) {
   new MiniprogramTemplatePlugin({ asyncChunkLoading: true }).apply(compiler)
   new FunctionModulePlugin().apply(compiler)
   new LoaderTargetPlugin('miniprogram').apply(compiler)
+  new ExternalsPlugin('commonjs2', [/^\/__wx__\//]).apply(compiler)
 }
