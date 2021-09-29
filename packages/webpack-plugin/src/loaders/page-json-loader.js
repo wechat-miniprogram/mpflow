b1f901273b6265d4836feefefa1dae305fe87f68
chyizheng <chyizheng@tencent.com>
2021-09-29 17:14:15 +0800

    feat(webpack-plugin): add componentGenerics support

--- packages/webpack-plugin/src/loaders/page-json-loader.js
+++ packages/webpack-plugin/src/loaders/page-json-loader.js
@@ -34,42 +34,54 @@ export default asyncLoaderWrapper(async function (source) {
 
   const { exports: moduleContent } = await evalModuleBundleCode(this, source, this.resource)
 
+  const usingComponents = []
+
   if (moduleContent.usingComponents) {
     // 对 comp.json 中读取到的 usingComponents 分别设立为入口
-    for (const componentRequest of Object.values(moduleContent.usingComponents)) {
-      if (!isRequest(componentRequest)) continue // 跳过 plugins:// 等等
-      if (isExternalModule && isExternalModule(componentRequest)) continue
+    usingComponents.push(...Object.values(moduleContent.usingComponents))
+  }
 
-      const resolvedComponentRequest = await resolveWithType(this, 'miniprogram/page', componentRequest)
-      const chunkName = getPageOutputPath(
-        this.rootContext,
-        appContext,
-        outputPath,
-        componentRequest,
+  if (moduleContent.componentGenerics) {
+    // 对 comp.json 中读取到的 componentGenerics 分别设立为入口
+    usingComponents.push(
+      ...Object.values(moduleContent.componentGenerics).map(componentGeneric => componentGeneric?.default),
+    )
+  }
+
+  for (const componentRequest of usingComponents) {
+    if (!componentRequest) continue
+    if (!isRequest(componentRequest)) continue // 跳过 plugins:// 等等
+    if (isExternalModule && isExternalModule(componentRequest)) continue // 跳过外部模块
+
+    const resolvedComponentRequest = await resolveWithType(this, 'miniprogram/page', componentRequest)
+    const chunkName = getPageOutputPath(
+      this.rootContext,
+      appContext,
+      outputPath,
+      componentRequest,
+      resolvedComponentRequest,
+    )
+
+    await addExternal(
+      this,
+      stringifyResource(
         resolvedComponentRequest,
-      )
-
-      await addExternal(
-        this,
-        stringifyResource(
-          resolvedComponentRequest,
-          [
-            {
-              loader: pageLoader,
-              options: {
-                appContext: appContext,
-                outputPath: chunkName,
-              },
+        [
+          {
+            loader: pageLoader,
+            options: {
+              appContext: appContext,
+              outputPath: chunkName,
             },
-            ...getMpflowLoaders(this, resolvedComponentRequest, 'page'),
-          ],
-          { disabled: 'normal' },
-        ),
-        'page',
-        chunkName,
-        chunkName,
-      )
-    }
+          },
+          ...getMpflowLoaders(this, resolvedComponentRequest, 'page'),
+        ],
+        { disabled: 'normal' },
+      ),
+      'page',
+      chunkName,
+      chunkName,
+    )
   }
 
   return '//'
